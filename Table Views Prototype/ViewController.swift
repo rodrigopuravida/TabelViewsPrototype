7f9161dd6ac37e51099e8f1faad3ac4aea4c7888
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2016-01-09 15:11:16 -0800

    Added delegate and datasource

--- Table Views Prototype/ViewController.swift
+++ Table Views Prototype/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    var cellContent = ["Rodrigo", "Heather", "Frankie", "yo yo"];
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellContent.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell");
+        cell.textLabel?.text = cellContent[indexPath.row];
+        
+        return cell;
     }
 
     override func didReceiveMemoryWarning() {
